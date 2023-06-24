81351995e7b8f26e6dad82426a7e58385acb2e0c
alkinemk <mazatkelian@yahoo.fr>
2023-06-25 00:49:22 +0200

    listings

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -79,6 +79,10 @@ export default async function handler(req: any, res: any) {
                     value: `<t:${webhook_data[0].timestamp}:R>`,
                     inline: true,
                   },
+                  {
+                    name: " ",
+                    value: " ",
+                  },
 
                   {
                     name: "Player points",
