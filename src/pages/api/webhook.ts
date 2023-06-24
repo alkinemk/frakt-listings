53fa377c5494c62d5046cff20afe977496e89670
alkinemk <mazatkelian@yahoo.fr>
2023-06-25 00:58:21 +0200

    listings

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -42,8 +42,6 @@ export default async function handler(req: any, res: any) {
 
       console.log("title", token.content.metadata.name);
       console.log("points_per_sol", points_per_sol);
-      console.log(typeof points_per_sol);
-      console.log(typeof playerPointsValue);
 
       if (points_per_sol <= 3) {
         const response = await fetch(webhook, {
