2a434ac6757d104cbebb80219e4b084281470d76
alkinemk <mazatkelian@yahoo.fr>
2023-07-01 13:59:11 +0200

    partner points support

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -19,10 +19,24 @@ const getAsset = async (token: string) => {
   return result;
 };
 
+// const { RestClient, FloorpriceBatchedRequest } = require("@hellomoon/api");
+
+// const client = new RestClient("d9fcf68d-11eb-4dd6-bbed-7f9c8007ed65");
+
+// client
+//   .send(
+//     new FloorpriceBatchedRequest({
+//       // ...your params
+//     })
+//   )
+//   .then(console.log)
+//   .catch(console.error);
+
 export default async function handler(req: any, res: any) {
   try {
     if (req.method === "POST") {
-      const webhook: any = process.env.DISCORD_WEBHOOK;
+      const player_webhook: any = process.env.DISCORD_WEBHOOK_PLAYER_POINTS;
+      const partner_webhook: any = process.env.DISCORD_WEBHOOK_PARTNER_POINTS;
 
       let webhook_data = req.body;
 
@@ -32,19 +46,27 @@ export default async function handler(req: any, res: any) {
         (item: any) => item.trait_type === "player points"
       );
 
+      let partnerPointsObject = token.content.metadata.attributes.find(
+        (item: any) => item.trait_type === "partner points"
+      );
+
       const playerPointsValue = playerPointsObject.value;
+      const partnerPointsValue = partnerPointsObject.value;
 
       let listing_price = (
         webhook_data[0].events.nft.amount / 1000000000
       ).toFixed(2);
 
-      const points_per_sol = Number(listing_price) / Number(playerPointsValue);
+      const player_points_per_sol =
+        Number(listing_price) / Number(playerPointsValue);
+      const partner_points_per_sol =
+        Number(listing_price) / Number(partnerPointsValue);
 
       console.log("title", token.content.metadata.name);
-      console.log("points_per_sol", points_per_sol);
+      console.log("points_per_sol", player_points_per_sol);
 
-      if (points_per_sol <= 3) {
-        const response = await fetch(webhook, {
+      if (player_points_per_sol <= 3) {
+        const response = await fetch(player_webhook, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -89,7 +111,7 @@ export default async function handler(req: any, res: any) {
                   },
                   {
                     name: "SOL per points",
-                    value: points_per_sol.toString(),
+                    value: player_points_per_sol.toFixed(2),
                     inline: true,
                   },
                 ],
@@ -106,22 +128,70 @@ export default async function handler(req: any, res: any) {
             ],
           }),
         });
-        // const response = await fetch(webhook, {
-        //   method: "POST",
-        //   headers: {
-        //     "Content-Type": "application/json",
-        //   },
-        //   body: JSON.stringify({
-        //     content: null,
-        //     embeds: [
-        //       {
-        //         title: token.content.metadata.name + " listed!",
-        //         url: `https://www.tensor.trade/item/${webhook_data[0].events.nft.nfts[0].mint}`,
-        //       },
-        //     ],
-        //   }),
-        // });
         console.log(response);
+      } else if (partner_points_per_sol <= 0.75) {
+        const response = await fetch(partner_webhook, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            content: null,
+            embeds: [
+              {
+                title: token.content.metadata.name + " listed!",
+                url: `https://www.tensor.trade/item/${webhook_data[0].events.nft.nfts[0].mint}`,
+                color: 16486972,
+                fields: [
+                  {
+                    name: " ",
+                    value: " ",
+                  },
+
+                  {
+                    name: " ",
+                    value: " ",
+                  },
+
+                  {
+                    name: ":moneybag:  Listing Price",
+                    value: "**" + listing_price + " " + "SOL**",
+                    inline: true,
+                  },
+                  {
+                    name: ":date:  Listing Date",
+                    value: `<t:${webhook_data[0].timestamp}:R>`,
+                    inline: true,
+                  },
+                  {
+                    name: " ",
+                    value: " ",
+                  },
+
+                  {
+                    name: "Player points",
+                    value: partnerPointsValue,
+                    inline: true,
+                  },
+                  {
+                    name: "SOL per points",
+                    value: partner_points_per_sol.toFixed(2),
+                    inline: true,
+                  },
+                ],
+                image: {
+                  url: token.content.files[0].uri,
+                },
+                timestamp: new Date().toISOString(),
+                footer: {
+                  text: "Helius",
+                  icon_url:
+                    "https://assets-global.website-files.com/641a8c4cac3aee8bd266fd58/642b5b2804ea37191a59737b_favicon-32x32.png",
+                },
+              },
+            ],
+          }),
+        });
       }
       res.status(200).json("success");
     }
